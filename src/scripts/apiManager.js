1112e2b983d9f7a0d91421c52e5a20961c570f48
Billy Mathison <wlmathison@gmail.com>
2019-03-12 14:45:55 -0500

    Adding getRestaurants to getCalls object in apiManager

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,3 +1,14 @@
 const getCalls = {
-
+    getRestaurants: function () {
+        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating", {
+                headers: {
+                    "Accept": "application/json",
+                    "user-key": "40ff6ce3387cdd36624832d171f31967"
+                }
+            })
+            .then(r => r.json())
+            .then(results => {
+                console.table(results)
+            })
+    }
 };
