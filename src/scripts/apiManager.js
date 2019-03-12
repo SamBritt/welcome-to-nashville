cf07563839a4c2d711fb9da819cf00537dd2bb4f
Jameka Echols <jameka.echols@gmail.com>
2019-03-12 15:05:21 -0500

    added parks

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,3 +1,7 @@
 const getCalls = {
-
+    getParks: function(){
+        fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
+        .then(response => response.json())
+        .then(parsedResponse => console.log(parsedResponse))
+    }
 };
