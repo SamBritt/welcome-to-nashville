ce34e22c7a42c0ce81b310ea4d7bc98beaac2569
SamBritt <sam.britt2@yahoo.com>
2019-03-13 08:56:17 -0500

    added get meetups fetch function

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,3 +1,11 @@
 const getCalls = {
+    getMeetups: function () {
+        fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville&location.address=nashville&token=AXUBYF6XR5MUANLU4KAI`, {
+            "headers": {
+                "Accept": "application/json"
+            }
+        }).then(response => response.json()).then(parsedResponse => console.log(parsedResponse))
+}
+}
 
-};
+getCalls.getMeetups();
