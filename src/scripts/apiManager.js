2bf860be8c54ae69c5e8a54dabe88670139b3561
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-13 09:43:55 -0500

    Fixed my fetch call to query Nashville differently and accept an argument of genre.

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -16,8 +16,8 @@ const getCalls = {
                 console.table(results)
             })
     },
-    getConcerts: function () {
-        return fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0")
+    getConcerts: function (genre) {
+        return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&city=Nashville&countryCode=US&classificationName=${genre}`)
             .then(response => response.json())
             .then(parsedResponse => console.log(parsedResponse))
     }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 getCalls.getParks()
-=======
 getCalls.getRestaurants()
->>>>>>> 22fabbca392c4e8936864939213a9aa1e6de4356
