74197895fdcc2cfa2c90a20a35775f8e1ed2406d
Billy Mathison <wlmathison@gmail.com>
2019-03-13 11:13:39 -0500

    changing div tag to article tag, appending h1 to mainContaier

--- src/scripts/domManager.js
+++ src/scripts/domManager.js
@@ -1,19 +1,25 @@
 const mainContainer = document.getElementById("display-container");
 
+// Appending h1 to mainContainer
+const buildPageHeader = document.createElement("H1");
+buildPageHeader.textContent = "Welcome to Nashville";
+mainContainer.appendChild(buildPageHeader)
+
 // Function to build section element with nested h2
-const buildSectionHTML = (sectionID) => {
+const buildSectionHTML = (sectionID, titleText) => {
     let section = document.createElement("section");
     section.id = (sectionID);
     let sectionTitle = document.createElement("H2");
+    sectionTitle.textContent = (titleText);
     section.appendChild(sectionTitle);
     return section;
 }
 
 // Appending input-container to mainContainer
-const inputContainer = mainContainer.appendChild(buildSectionHTML("input-container"))
+const inputContainer = mainContainer.appendChild(buildSectionHTML("input-container", "Search for stuff to do today"));
 
 // Appending results-container to mainContainer
-const resultsContainer = mainContainer.appendChild(buildSectionHTML("results-container"))
+const resultsContainer = mainContainer.appendChild(buildSectionHTML("results-container", "Results"));
 
 // Appending itinerary-container to mainContainer
-const itineraryContainer = mainContainer.appendChild(buildSectionHTML("itinerary-container"))
+const itineraryContainer = mainContainer.appendChild(buildSectionHTML("itinerary-container", "My itinerary"));
