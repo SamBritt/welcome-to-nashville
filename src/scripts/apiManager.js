2e52fa9b82bba7273bd9d47d5197e02b8fec169b
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-13 14:44:23 -0500

    Fixed some things in my fetch call.

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,34 +1,34 @@
 const getCalls = {
-    getMeetups: function () {
-       return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville&location.address=nashville&token=AXUBYF6XR5MUANLU4KAI`, {
-            "headers": {
-                "Accept": "application/json"
-            }
-        }).then(response => response.json())
-    },
-
-    getParks: function () {
-        fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
-            .then(response => response.json())
-            .then(parsedResponse => console.log(parsedResponse))
-    },
-
-    getRestaurants: function () {
-        fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating", {
-                headers: {
-                    "Accept": "application/json",
-                    "user-key": "40ff6ce3387cdd36624832d171f31967"
+        getMeetups: function () {
+            return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville&location.address=nashville&token=AXUBYF6XR5MUANLU4KAI`, {
+                "headers": {
+                    "Accept": "application/json"
                 }
-            })
-            .then(r => r.json())
-            .then(results => {
-                console.table(results)
-            })
-    },
+            }).then(response => response.json())
+        },
 
-    getConcerts: function (genre) {
-        return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&size=4&city=Nashville&countryCode=US&classificationName=${genre}`)
-            .then(response => response.json())
-            .then(parsedResponse => console.log(parsedResponse._embedded.events))
-    }
-};
+        getParks: function () {
+            fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
+                .then(response => response.json())
+                .then(parsedResponse => console.log(parsedResponse))
+        },
+
+        getRestaurants: function () {
+            fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating", {
+                    headers: {
+                        "Accept": "application/json",
+                        "user-key": "40ff6ce3387cdd36624832d171f31967"
+                    }
+                })
+                .then(r => r.json())
+                .then(results => {
+                    console.table(results)
+                })
+        },
+
+        getConcerts: function (genre) {
+            return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&size=4&city=Nashville&countryCode=US&classificationName=${genre}`)
+                .then(response => response.json())
+                .then(concertsArray => concertsArray._embedded.events)
+        }
+}
